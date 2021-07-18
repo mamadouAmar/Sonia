c13b3e3629e9c500fe61b31eabe799ddd24f700c
quantumAmar <dmamadouamar@ept.sn>
2021-07-18 17:29:45 +0000

    Immplémentation du code HTML

--- home/views.py
+++ home/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .views import *
 
 # Create your views here.
+def afficher_home(request):
+    return render(request, "index.html")
--- products/models.py
+++ products/models.py
@@ -1,23 +1,26 @@
 from django.db import models
+from django.utils import timezone
+from datetime import datetime
 
 # Create your models here.
 class Produit(models.Model):
-    libelle = models.CharField((""), max_length=100)
-    prix = models.FloatField((""))
-    description = models.TextField((""))
+    libelle = models.CharField(("libelle"), max_length=100)
+    prix = models.FloatField(("prix"))
+    mode_d_emploi = models.TextField(("mode_d_emploi"))
+    ingredients = models.TextField(("ingredients"))
 
     class Meta:
         verbose_name = "Produit"
         verbose_name_plural = "Produits"
 
     def __str__(self):
-        return self.name
+        return self.libelle
 
 
 class Assets(models.Model):
-    nom = models.CharField((""), max_length=50)
-    date_ajout = models.DateField((""), auto_now=False, auto_now_add=False)
-    produit = models.ForeignKey("products.Produit", verbose_name=(""), on_delete=models.CASCADE)
+    asset = models.ImageField(("image"), upload_to=None, height_field=None, width_field=None, max_length=None)    
+    date_ajout = models.DateTimeField(("date"), auto_now=False, auto_now_add=False, default = timezone.now)
+    produit = models.ForeignKey("Produit", verbose_name=("produit"), on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = "Asset"

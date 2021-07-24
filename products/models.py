122a148a7bc73d8bb3c9abd58b82ac635a7eafda
quantumAmar <dmamadouamar@ept.sn>
2021-07-24 18:02:24 +0000

    ajout des assets

--- products/models.py
+++ products/models.py
@@ -6,8 +6,8 @@ from datetime import datetime
 class Produit(models.Model):
     libelle = models.CharField(("libelle"), max_length=100)
     prix = models.FloatField(("prix"))
-    mode_d_emploi = models.TextField(("mode_d_emploi"))
-    ingredients = models.TextField(("ingredients"))
+    mode_d_emploi = models.TextField(("mode d'emploi"), default = "", blank = True)
+    ingredients = models.TextField(("ingredients"), default = "", blank = True)
 
     class Meta:
         verbose_name = "Produit"
@@ -18,7 +18,7 @@ class Produit(models.Model):
 
 
 class Assets(models.Model):
-    asset = models.ImageField(("image"), upload_to=None, height_field=None, width_field=None, max_length=None)    
+    asset = models.ImageField(("image"), upload_to=None, height_field=None, width_field=None, max_length=None, default="")    
     date_ajout = models.DateTimeField(("date"), auto_now=False, auto_now_add=False, default = timezone.now)
     produit = models.ForeignKey("Produit", verbose_name=("produit"), on_delete=models.CASCADE)
 
@@ -27,7 +27,7 @@ class Assets(models.Model):
         verbose_name_plural = "Assets"
 
     def __str__(self):
-        return nom
+        return str(self.date_ajout)
 
     
 

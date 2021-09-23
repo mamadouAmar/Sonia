950d7b7f450e78d221bdb8e88ea8c8b2c8402074
quantumAmar <dmamadouamar@ept.sn>
2021-09-23 14:59:28 +0000

    ajout des fichiers statques

--- home/views.py
+++ home/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from .views import *
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 def afficher_home(request):
-    return render(request, "index.html")
+
+    template = loader.get_template('sonia/index.html')
+    return HttpResponse(template.render(request = request))

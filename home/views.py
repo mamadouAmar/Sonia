2a7ba3708e97f3039db164cc94543f371c3aa5d9
quantumAmar <dmamadouamar@ept.sn>
2021-11-10 11:30:12 +0000

    chore: définition des templates

--- home/views.py
+++ home/views.py
@@ -4,6 +4,5 @@ from django.template import loader
 
 # Create your views here.
 def afficher_home(request):
-
     template = loader.get_template('index.html')
     return HttpResponse(template.render(request = request))

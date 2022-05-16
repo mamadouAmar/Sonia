b69ddae8949ce791c65e406d9d9f88ef31f3740b
quantumAmar <dmamadouamar@ept.sn>
2022-05-16 18:27:16 +0000

    trying to make the images visible

--- sonia/settings.py
+++ sonia/settings.py
@@ -103,7 +103,7 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] = dj_database_url.config(conn_max_age = 600)
+DATABASES['default'] = dj_database_url.config()
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 

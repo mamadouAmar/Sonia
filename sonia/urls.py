a7745c0fd28667c6d86788c199f44528374ff46c
quantumAmar <dmamadouamar@ept.sn>
2022-05-16 12:44:05 +0000

    trying to solve internal server error

--- sonia/urls.py
+++ sonia/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('', include('home.urls')),
     path('produits/', include('products.urls'), name = 'produit'),
     path('dejeuner/', include('dejeuner.urls'), name = "dejeuner"),
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 

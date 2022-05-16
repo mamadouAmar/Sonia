f4ae50286ac5bb79533ac1be434d4ca3af7c959f
quantumAmar <dmamadouamar@ept.sn>
2022-05-16 14:35:44 +0000

    trying to make the images visible

--- sonia/settings.py
+++ sonia/settings.py
@@ -147,6 +147,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, '')
 MEDIA_URL = '/'
